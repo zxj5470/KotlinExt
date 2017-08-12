695a7af303a46ee90980da0ad2a278133233018e
zxj5470 <zxj5470@foxmail.com>
2017-08-12 09:03:49 +0800

    Initial commit

---
+++ android-kotlin-ext/src/main/java/com/github/zxj5470/android_kotlin_ext/str/StringExt.kt
@@ -0,0 +1,30 @@
+package com.github.zxj5470.android_kotlin_ext.str
+
+import java.util.*
+
+/**
+ * count source string appeared times in source string
+ * @param regex String : the string to match source string
+ * @return List<Int>: a list of all match indices
+ */
+fun String.indicesOf(regex: String): List<Int> {
+    val ll = LinkedList<Int>()
+    var temp = -1
+    var b: Int
+    this.forEachIndexed { index, _ ->
+        b = this.indexOf(regex, index)
+        if (b > temp) {
+            temp = b
+            ll.add(temp)
+        }
+    }
+    return ll.toList()
+}
+
+/**
+ * count source string appeared times in source string
+ * @param regex String : the string to match source string
+ * @return Int: count how many times appeared.
+ * @author :zxj5470
+ */
+fun String.countTimes(regex: String)=this.indicesOf(regex).size
