51c5a5ce894c9d8868d923ffbf9a4c6692fb0033
zxj5470 <zxj5470@foxmail.com>
2017-08-15 09:30:37 +0800

    v0.1.4 fixed the `log`

--- android-kotlin-ext/src/main/java/com/github/zxj5470/android_kotlin_ext/log/LogExt.kt
+++ android-kotlin-ext/src/main/java/com/github/zxj5470/android_kotlin_ext/log/LogExt.kt
@@ -3,31 +3,21 @@ package com.github.zxj5470.android_kotlin_ext.log
 import android.util.Log
 
 /**
- * @param TAG String :the name of javaClass for Any
- * @param content String :the content(value) of Any
+ * @param TAG String :the name of javaClass for Any [this.javaClass.toString()]
+ * @param content String :the content(value) of Any [this.toString()]
  */
-inline fun Any.logE() = this.logTagE(this.javaClass.toString())
-
-/**
- * @param TAG String : input your TAG
- */
-inline fun Any.logTagE(TAG: String) = Log.e(TAG, this.toString())
+inline fun Any.logE(TAG: String=this.javaClass.toString()) = Log.e(TAG, this.toString())
 
 /**
  * the methods below is as the same.
  * skip to introduce them.
  */
-inline fun Any.logI() = this.logTagI(this.javaClass.toString())
-inline fun Any.logTagI(TAG: String) = Log.i(TAG, this.toString())
+inline fun Any.logI(TAG: String=this.javaClass.toString()) = Log.i(TAG, this.toString())
 
-inline fun Any.logD() = this.logTagD(this.javaClass.toString())
-inline fun Any.logTagD(TAG: String) = Log.d(TAG, this.toString())
+inline fun Any.logD(TAG: String=this.javaClass.toString()) = Log.d(TAG, this.toString())
 
-inline fun Any.logV() = this.logTagV(this.javaClass.toString())
-inline fun Any.logTagV(TAG: String) = Log.v(TAG, this.toString())
+inline fun Any.logV(TAG: String=this.javaClass.toString()) = Log.v(TAG, this.toString())
 
-inline fun Any.logW() = this.logTagW(this.javaClass.toString())
-inline fun Any.logTagW(TAG: String) = Log.w(TAG, this.toString())
+inline fun Any.logW(TAG: String=this.javaClass.toString()) = Log.w(TAG, this.toString())
 
-inline fun Any.logWTF() = this.logTagWTF(this.javaClass.toString())
-inline fun Any.logTagWTF(TAG: String) = Log.wtf(TAG, this.toString())
+inline fun Any.logWTF(TAG: String=this.javaClass.toString()) = Log.wtf(TAG, this.toString())
